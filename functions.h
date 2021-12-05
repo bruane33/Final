96c03534b10bc8883a4cd21896e674eac1dc5f77
RobotPoundcake <bruane33@gmail.com>
2021-12-05 16:18:39 -0600

    changes made

--- functions.h
+++ functions.h
@@ -4,24 +4,15 @@
 #include <map>
 #include <queue>
 
-Media addMedia();
+void addMedia(std::map<int,Media>&, std::priority_queue<int, std::vector<int>, std::greater<int>>&, std::vector<int>&);
 void listAll(std::map<int,Media>&);
 void searchByName(std::map<int,Media>&);
 void searchByGenre(std::map<int,Media>&);
 void searchBySystem(std::map<int,Media>&);
-void searchByID(std::map<int,Media> &x);
-int removeMedia(std::map<int,Media> &x);
-int searchUnplayed(std::map<int,Media>&, std::priority_queue<int, std::vector<int>, std::greater<int>>&);
+void searchByID(std::map<int,Media>&);
+void removeMedia(std::map<int,Media>&, std::priority_queue<int, std::vector<int>, std::greater<int>>&);
+void searchUnplayed(std::map<int,Media>&, std::priority_queue<int, std::vector<int>, std::greater<int>>&);
 void listAllUnplayed(std::map<int,Media>&, std::priority_queue<int, std::vector<int>, std::greater<int>>&);
-
-
-
-
-
-
-
-
-
-
+void styleLine1();
 
 #endif // FUNCTIONS_H
--- main.cpp
+++ main.cpp
@@ -1,3 +1,19 @@
+/**************************************************************
+* Name        : Final Project - The Ruane Database
+* Author      : Brian Ruane
+* Created     : 10/06/2021
+* Course      : CIS 152 – Data Structures
+* Version     : 1.0
+* OS          : Windows 10
+* Copyright   : This is my own original work based on
+*               specifications issued by our instructor
+* Description : This program creates a queue (created using list functions) and demonstrates
+*               some of the queue's functionality.
+* Academic Honesty: I attest that this is my original work.
+* I have not used unauthorized source code, either modified or
+* unmodified. I have not given other fellow student(s) access to
+* my program.
+***************************************************************/
 #include <map>
 #include <queue>
 #include <iostream>
@@ -15,8 +31,8 @@
 
 int main()
 {
-    //DECLARATIONS
-    int input;
+    //PRELIMINARY DECLARATIONS
+    int input = 1;
     std::map<int,Media> database;
     std::vector <int> keyID;
     keyID.push_back(0);
@@ -48,25 +64,10 @@ int main()
     reader.close();
 
 
-    std::ifstream reader2("C:/Users/Brian/Documents/CIS152 Data Structures/Final/unplayed.txt");
-    if(!reader2.is_open()){
-        std::cout << "Error opening database file." << std::endl;
-    }
-    while(!reader2.eof()){
-        Media temp;
-        std::string isPlayed;
-        getline(reader2, temp.title);
-        getline(reader2, temp.genre);
-        getline(reader2, temp.system);
-        getline(reader2, isPlayed);
-    }
-    reader2.close();
-
-
-
-//INTRODUCFTION
-    std::cout << "Welcome to the Ruane Media Database!\nChoose an option to get started:\n";
+    //MAIN MENU DISPLAY AND INPUT
     while (input != 0){
+    std::cout << "The Ruane Media Database!\nChoose an option to continue:\n";
+    styleLine1();
     std::cout << "1:Add Media\n2:Remove Media\n3:Next Unplayed Title\n4:Search By Name\n5:Search By Genre\n6:Search by System\n7:Search by ID\n8:List All\n9:List All Unplayed\n0:Quit\n";
     std::cin >> input;
     std::cin.ignore();
@@ -77,124 +78,46 @@ int main()
         std::cin >> input;
         std::cin.ignore();
     }
+
     //MAIN MENU OPTIONS
     switch(input){
     case 1:{
-        system("CLS");// ADD MEDIA
-        Media temp;
-        std::cout << "ADD MEDIA" << std::endl;
-        temp = addMedia();
-        database.insert({keyID.back(), temp});
-        unplayedQueue.push(keyID.back()); //add new added media to queue of unplayed
-        keyID.push_back(keyID.back()+1);
-        std::ofstream writer;
-        std::ofstream writer2;
-        writer.open("C:/Users/Brian/Documents/CIS152 Data Structures/Final/saveditems.txt",std::ios::app);
-        writer << "\n" << temp.title << "\n";
-        writer << temp.genre << "\n";
-        writer << temp.system << "\n";
-        writer << temp.played << "\n";
-        writer.close();
-        writer2.open("C:/Users/Brian/Documents/CIS152 Data Structures/Final/unplayed.txt",std::ios::app);
-        writer2 << "\n" << temp.title << "\n";
-        writer2 << temp.genre << "\n";
-        writer2 << temp.system << "\n";
-        writer2.close();
+        addMedia(database,unplayedQueue,keyID);
         break;
     }
     case 2:{
-        system("CLS");//REMOVE MEDIA
-        Media temp;
-        int erase = removeMedia(database);
-        std::ofstream writer3;
-        writer3.open("C:/Users/Brian/Documents/CIS152 Data Structures/Final/saveditems.txt", std::ofstream::out | std::ofstream::trunc);
-        writer3.close();
-        writer3.open("C:/Users/Brian/Documents/CIS152 Data Structures/Final/saveditems.txt",std::ios::app);
-        for(auto& find : database){
-            if (find.first != erase){
-                writer3 << find.second.title << "\n";
-                writer3 << find.second.genre << "\n";
-                writer3 << find.second.system << "\n";
-                writer3 << find.second.played << "\n";
-            }
-        }
-        database.erase(erase);
+        removeMedia(database,unplayedQueue);
         break;
     }
     case 3:{
-        system("CLS");//NEXT UNPLAYED
-        int result = searchUnplayed(database, unplayedQueue);
-        int played;
-        std::cout << "\n1:Finished Game\n2:Next Title\n3:Back to Main\n" << std::endl;
-        std::cin >> played;
-        while (!std::cin || played < 1 || played > 3){
-            std::cout << "Input must be integer 1 through 3. Please try again.\n";
-            std::cin >> played;
-            std::cin.ignore();
-        }
-        switch (played){
-        case 1:{
-            for(auto& find : database){
-                if(find.first == result){
-                    find.second.played = true;
-                }
-            }
-            std::ofstream writer4;
-            writer4.open("C:/Users/Brian/Documents/CIS152 Data Structures/saveditems.txt", std::ofstream::out | std::ofstream::trunc);
-            writer4.close();
-            writer4.open("C:/Users/Brian/Documents/CIS152 Data Structures/saveditems.txt",std::ios::app);
-            for(auto& find : database){
-                    writer4 << find.second.title << "\n";
-                    writer4 << find.second.genre << "\n";
-                    writer4 << find.second.system << "\n";
-                    if(find.second.played == true){
-                        writer4 << "true" << "\n";
-                    }else{
-                        writer4 << "false" << "\n";
-                    }
-            }
-            unplayedQueue.pop();
-            std::cout << "Game removed from unplayed queue!\n" << std::endl;
-            break;
-        }
-        case 3:{
-            break;
-        }
-        }
-
+        searchUnplayed(database,unplayedQueue);
         break;
     }
-    case 4: //SEARCH BY NAME
-        system("CLS"); //Clear Screen
+    case 4:{
         searchByName(database);
-        break;//more changes
-    case 5:
-        system("CLS");//SEARCH BY GENRE
+        break;
+    }
+    case 5:{
         searchByGenre(database);
         break;
-    case 6:
-        system("CLS");//LIST ALL
+    }
+    case 6:{
         searchBySystem(database);
         break;
-    case 7:
-        system("CLS");
+    }
+    case 7:{
         searchByID(database);
         break;
-    case 8:
-        system("CLS");
+    }
+    case 8:{
         listAll(database);
         break;
+    }
     case 9:
-        //listAllUnplayed(database,unplayedQueue);
-        for (int i = 0; i < unplayedQueue.size(); ++i){
-            std::cout << unplayedQueue.top() << std::endl;
-            unplayedQueue.pop();
-        }
+        listAllUnplayed(database,unplayedQueue);
         break;
     }
-    //system("CLS");
     reader.close();
-    reader2.close();
     }
     return 0;
 }
